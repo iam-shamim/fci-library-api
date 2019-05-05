fd25c88e821a64c2d70b0d31f5cd016996b11ea5
shamim <shamim258038@gmail.com>
2019-05-06 04:15:19 +0600

    category model bug fix

--- src/Models/CategoryModel.js
+++ src/Models/CategoryModel.js
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose';
 var Schema = mongoose.Schema;
 var CategorySchema = new Schema({
-    _id: Schema.Types.ObjectId,
     name: String,
     created_at: {
         type: Date,
